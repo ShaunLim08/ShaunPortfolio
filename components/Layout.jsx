d7e756c636cb3355c267064d3e2d4f555652dbc5
Shaun Lim <slim12252@gmail.com>
2024-09-01 04:26:59 +0800

    test favicon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
 
+export const metadata = {
+    title: "Shaun's Website",
+    descrtipion: "This is a website for Shaun",
+    icons: {
+        icon: "/potato.png",
+    },
+}
+
 const Layout = ({ children }) => {
     return(
         <div className="min-h-screen mx-auto ">
+            {children}
             <Navbar />
             <main className=" pl-6 pr-6  md:pl-12 md:pr-12 lg:pr-12 lg:pl-12">{children}</main>
         </div>
