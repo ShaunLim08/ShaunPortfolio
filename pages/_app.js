6412fdaa87f81af9f45abe7f381e00d1a6baf178
Shaun Lim <slim12252@gmail.com>
2024-09-13 12:54:28 +0800

    Revert "tried to add dark and light mode"
    This reverts commit e743c725d1d4970e3e5554bda776eb06d2a60230.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,11 @@
 import '@/styles/globals.css'
 import Layout from '../components/Layout'
-import { ThemeProvider } from "@/components/theme-provider"
 
 export default function App({ Component, pageProps }) {
+
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
   );
-}
+};
